84c623d8e947fb2ac35ab7265db9f2cb574ee8a8
brewxxx <98697422+brewxxx@users.noreply.github.com>
2022-02-21 05:43:48 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,35 +1,54 @@
-const userInfo = document.querySelector("#login");
-const greeting = document.querySelector("#greeting");
-const loginForm = document.querySelector("#login-form");
+const userInfo = document.querySelector('#login');
+const greeting = document.querySelector('#greeting');
+const loginForm = document.querySelector('#login-form');
 
-const HIDDEN_KEY = "hidden";
-const USER_KEY = "userName";
+const HIDDEN_KEY = 'hidden';
+const USER_KEY = 'userName';
 
-function showGreeting() {
-    loginForm.classList.add(HIDDEN_KEY);
-    greeting.innerText = `Hello, ${userInfo.value}`;
-    greeting.classList.remove(HIDDEN_KEY);
+function showGreeting(hello) {
+  loginForm.classList.add(HIDDEN_KEY);
+  greeting.innerText = `${hello}, ${userInfo.value}`;
+  greeting.classList.remove(HIDDEN_KEY);
 }
 
 function saveUserInfo() {
-    localStorage.setItem(USER_KEY, userInfo.value);
+  localStorage.setItem(USER_KEY, userInfo.value);
 }
 
-function checkUser(){
-    const savedUser = localStorage.getItem(USER_KEY);
-    if(savedUser !== null){
-        loginForm.classList.add(HIDDEN_KEY);
-        greeting.innerText = `Hello, ${savedUser}`;
-        greeting.classList.remove(HIDDEN_KEY);
+function checkUser(hello) {
+  const savedUser = localStorage.getItem(USER_KEY);
+  if (savedUser !== null) {
+    loginForm.classList.add(HIDDEN_KEY);
+    greeting.innerText = `${hello}, ${savedUser}`;
+    greeting.classList.remove(HIDDEN_KEY);
+  }
+}
+
+
+function sayHello() {
+    const date = new Date();
+    const hour = date.getHours();
+    let hello = 'hello';
+    if (hour >= 21 || hour < 6) {
+        hello = 'Good night';
+    } else if (hour >= 12 && hour < 18) {
+        hello = 'Good afternoon';
+    } else if (hour >= 6 && hour < 12) {
+        hello = 'Good morning';
+    } else {
+        hello = 'Good evening';
     }
+    checkUser(hello);
+    console.log(hour);
 }
 
-checkUser();
+sayHello();
 
-function preventSubmit(event){
-    event.preventDefault();
-    showGreeting();
-    saveUserInfo();
+function preventSubmit(event) {
+  event.preventDefault();
+  showGreeting();
+  saveUserInfo();
+  sayHello();
 }
 
-loginForm.addEventListener("submit", preventSubmit);
+loginForm.addEventListener('submit', preventSubmit);
